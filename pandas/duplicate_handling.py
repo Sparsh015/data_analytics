85ba37b364af9b0275c4d53a84ba8cfb4fa8a8a8
sparsh15 <sparsh.1518@gmail.com>
2025-05-11 19:53:53 +0530

    duplicate handling

--- pandas/duplicate_handling.py
+++ pandas/duplicate_handling.py
@@ -1,1 +1,17 @@
 import pandas as pd
+
+data = pd.read_excel("D:/Python/data_analytics/IIT_R UNGA.xlsx")
+print("data\n",data)
+
+# duplicate values in row
+print(data.duplicated())
+
+print()
+
+#duplicate value in columm
+print(data["Countries"].duplicated())
+
+#count of duplicate data
+print(data["Countries"].duplicated().sum())
+
+print(data.drop_duplicates("MOD 1 (10)"))
